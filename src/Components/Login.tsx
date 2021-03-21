4f3e82053ded663fb631404b24f38faabee4622b
keshan-spec <keshanth.programmer@gmail.com>
2021-03-21 09:15:51 +0530

    Login v2 with Error messages

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -12,19 +12,23 @@ export const Login: React.FC<Props> = ({ authState }) => {
   const username = useRef<HTMLInputElement | null>(null)
   const password = useRef<HTMLInputElement | null>(null)
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<{ error: boolean, message: string }>({ error: false, message: "" });
 
 
   const authenticate_user = () => {
     if (!username.current?.value) {
       username.current?.focus()
+      setError({ error: true, message: "Username is required!" })
       return
     }
     else if (!password.current?.value) {
       password.current?.focus()
+      setError({ error: true, message: "Password is required!" })
       return
     }
 
     setLoading(true)
+
     const data = { username: username.current.value, password: password.current.value }
 
     axios({
@@ -33,13 +37,13 @@ export const Login: React.FC<Props> = ({ authState }) => {
       headers: {},
       data
     }).then(res => {
-      console.log(res.data.token);
       authState(true)
       document.cookie = `dogfood=${res.data.token}`
     }).catch(err => {
       console.log(err);
       password.current!.value = ""
       setLoading(false)
+      setError({ error: true, message: "Incorrect username or password!" })
     })
 
     return
@@ -55,6 +59,7 @@ export const Login: React.FC<Props> = ({ authState }) => {
         <button>Get started!</button>
       </div>
       <div className="form">
+        <span className={error.error ? "error" : "hidden"}>{error.message}</span>
         <input type="text" placeholder="Enter username" ref={username} />
         <input type="password" placeholder="Enter password" ref={password} />
         <button onClick={authenticate_user} disabled={loading}>
